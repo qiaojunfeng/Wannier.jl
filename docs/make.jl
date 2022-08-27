e6d9f5a8fee721f7390d9f6f1f88497afb8667d1
Junfeng Qiao <qiaojunfeng@outlook.com>
2022-08-27 13:39:36 +0200

    [doc] Add scaffolding

--- docs/make.jl
+++ docs/make.jl
@@ -9,6 +9,15 @@ makedocs(;
         "Home" => "index.md",
         "Tutorial" => "tutorial.md",
         "Theory" => ["Normalization" => "theory/normalization.md"],
-        "API" => ["Input/Output" => "api/io.md", "Real space" => "api/realspace.md"],
+        "API" => [
+            "Utilities" => "api/util.md",
+            "Input/Output" => "api/io.md",
+            "B vectors" => "api/bvector.md",
+            "Model" => "api/model.md",
+            "Wannierize" => "api/wannierize.md",
+            "Interpolation" => "api/interpolation.md",
+            "Real space" => "api/realspace.md",
+            "Command line" => "api/cli.md",
+        ],
     ],
 )
---
+++ docs/src/api/cli.md
@@ -0,0 +1,26 @@
+# Command line interface
+
+A simple command line interface is generated with
+[`Comonicon.jl`](https://github.com/comonicon/Comonicon.jl).
+
+```@meta
+CurrentModule = Wannier
+```
+
+## Index
+
+```@index
+Pages = ["cli.md"]
+```
+
+## Commands
+
+```@docs
+maxloc
+dis
+ptg
+optrot
+splitvc
+trunc
+band
+```
