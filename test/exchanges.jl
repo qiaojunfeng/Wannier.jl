1460a64214800868676e9c40a01fbd547e3f5c51
Louis Ponet <louisponet@gmail.com>
2023-08-04 10:14:50 +0200

    added back exchanges

---
+++ test/exchanges.jl
@@ -0,0 +1,23 @@
+# A reusable fixture for a model
+# no disentanglement
+
+@testitem "exchanges" begin
+    using LinearAlgebra
+    using Wannier: Vec3
+    model_up = read_w90(joinpath(FIXTURE_PATH, "iron", "up"))
+    model_dn = read_w90(joinpath(FIXTURE_PATH, "iron", "dn"))
+    Mupdn = read_amn(joinpath(FIXTURE_PATH, "iron", "iron.mud"))
+    model = Wannier.MagModel(model_up, model_dn, Mupdn)
+    hami = Wannier.TBHamiltonian(model)
+
+    atoms = Wannier.Atoms(model)
+
+    n_ωh = 300
+    n_ωv = 50
+    nk = (5, 5, 5)
+    exch = Wannier.calc_exchanges(hami, atoms, 0.01; R = Vec3(0,0,1), n_ωh, n_ωv, nk)
+
+    @code_warntype Wannier.ExchangeKGrid(hami, Wannier.uniform_shifted_kgrid(nk...), R)
+    maxJ = maximum([tr(e.J) for e in exch])
+    @test isapprox(maxJ, -158.90552565580805)
+end
