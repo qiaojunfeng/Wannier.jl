41350b2b1c6b6d4bab2a7067e512435905cc66c4
Junfeng Qiao <qiaojunfeng@outlook.com>
2023-11-09 14:23:53 +0100

    Plot DOS

--- ext/WannierPlotlyJSExt.jl
+++ ext/WannierPlotlyJSExt.jl
@@ -486,4 +486,96 @@ function Wannier.plot_crystal_structure(
     return PlotlyJS.plot(P)
 end
 
+function Wannier.get_dos_plot(
+    x::AbstractVector,
+    y::AbstractVector;
+    fermi_energy=nothing,
+    shift_fermi::Bool=false,
+    xlabel="Energy (eV)",
+    ylabel="DOS (states/eV)",
+)
+    traces = PlotlyJS.AbstractTrace[]
+
+    t = PlotlyJS.scatter(;
+        x,
+        y,
+        mode="lines",
+        # line=(; color=c, kwargs...)
+    )
+    push!(traces, t)
+
+    layout = PlotlyJS.Layout(;
+        showlegend=false,
+        xaxis=PlotlyJS.attr(;
+            range=[x[1], x[end]],
+            title=xlabel,
+            zeroline=false,
+            showgrid=true,
+            showbackground=false,
+            # gridcolor = KLINE_COL[],
+            ticks="outside",
+            showline=true,
+            mirror=true,
+            linecolor="black", # show axis boundary
+        ),
+        yaxis=PlotlyJS.attr(;
+            range=[minimum(y) - 0.5, maximum(y) + 0.5],
+            title=ylabel,
+            zeroline=false,
+            showgrid=false,
+            showbackground=false,
+            ticks="outside",
+            showline=true,
+            mirror="all",
+            linecolor="black", # show axis boundaries on all subplots
+        ),
+        hovermode="closest",
+        autosize=true,
+        # width = 480, height = 480,
+        # #margin=attr(l=50, r=5, b=15, t=10),
+        plot_bgcolor="rgba(0,0,0,0)",  # transparent background
+        paper_bgcolor="rgba(0,0,0,0)",  # transparent background
+    )
+
+    shapes = []
+    # add horizontal line for y = 0
+    push!(
+        shapes,
+        PlotlyJS.hline(
+            0;
+            mode="lines",
+            line=PlotlyJS.attr(; dash="dash", color="grey", width=0.2),
+        ),
+    )
+    if fermi_energy !== nothing
+        if shift_fermi
+            x .-= fermi_energy
+            ylabel = "E - E_F (eV)"
+            εF_plot = 0
+        else
+            εF_plot = fermi_energy
+        end
+        # add vertical line for Fermi to the background
+        push!(
+            shapes,
+            PlotlyJS.vline(
+                εF_plot;
+                mode="lines",
+                line=PlotlyJS.attr(; dash="dash", color="blue", width=0.2),
+            ),
+        )
+    end
+    PlotlyJS.relayout!(layout; shapes)
+
+    return PlotlyJS.Plot(traces, layout)
+end
+
+"""
+Plot density of states.
+"""
+function Wannier.plot_dos(x::AbstractVector, y::AbstractVector; kwargs...)
+    P = Wannier.get_dos_plot(x, y; kwargs...)
+    return PlotlyJS.plot(P)
+end
+
 end
--- src/plot.jl
+++ src/plot.jl
@@ -1,4 +1,4 @@
-export plot_band, plot_band_diff, plot_crystal_structure
+export plot_band, plot_band_diff, plot_crystal_structure, plot_dos
 
 """
 Plot band structure.
@@ -18,5 +18,7 @@ function get_band_plot end
 function get_band_diff_plot end
 
 function get_crystal_structure_plot end
-
 function plot_crystal_structure end
+
+function get_dos_plot end
+function plot_dos end
