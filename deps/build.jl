aefe2ee74d82fa6d3e91858d8b4b3383a1aa7f98
Junfeng Qiao <qiaojunfeng@outlook.com>
2024-01-30 20:38:42 +0100

    Remove Comonicon CLI
    The CLI are moved into a separate repo at
    https://github.com/qiaojunfeng/WannierCLI.jl
    
    This reduces compilation time & allows easier changes to the CLI.

--- deps/build.jl
+++
@@ -1,6 +0,0 @@
-using Wannier
-
-# See possible options by
-# julia --project deps/build.jl -h
-
-# Wannier.comonicon_install()
--- test/cli/help.jl
+++
@@ -1,8 +0,0 @@
-
-@testitem "cli help" begin
-    @test Wannier.command_main(["-h"]) == 0
-
-    for cmd in ["maxloc", "dis", "ptg", "optrot", "splitvc", "fermisurf"]
-        @test Wannier.command_main([cmd, "-h"]) == 0
-    end
-end
