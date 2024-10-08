fdc42368eecd08418264496ad815e17647551769
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-10-08 13:35:50 +0200

    Disable wannierize/coopt tests due to missing iron fixtures

--- test/runtests.jl
+++ test/runtests.jl
@@ -33,7 +33,8 @@ mat2vec(A::AbstractMatrix) = [v for v in eachcol(A)]
     include("wannierize/parallel_transport.jl")
     include("wannierize/constrain_center/max_localize.jl")
     include("wannierize/constrain_center/disentangle.jl")
-    include("wannierize/coopt.jl")
+    # TODO: iron fixtures are missing from the repo
+    # include("wannierize/coopt.jl")
 
     include("realspace.jl")
 
