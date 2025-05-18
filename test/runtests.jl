e75841df89b5601ff1ab983d66eeb60866b7f16c
Junfeng Qiao <qiaojunfeng@outlook.com>
2025-05-18 18:02:51 +0200

    Clean up deps

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,6 @@
-# using Aqua
-# only test ambiguities in current module, otherwise fails due to ambiguities in other packages
-#   https://github.com/JuliaTesting/Aqua.jl/issues/77
-# disable project_extras since we don't use julia < 1.2
-# Aqua.test_all(Wannier; ambiguities=false, project_extras=false)
-# Aqua.test_ambiguities(Wannier)
-
 using TestItemRunner
 
-# TODO enable everything once the refactor is done
-@run_package_tests verbose = true filter = ti -> occursin("interpolation/", ti.filename)
+# Filter test files
+@run_package_tests verbose = true filter = ti -> occursin("interpolation", ti.filename)
+
+# @run_package_tests verbose = true
