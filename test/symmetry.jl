9a237ad4bd56af481aca96fd179cc949758e3845
Junfeng Qiao <qiaojunfeng@outlook.com>
2026-01-28 20:20:49 +0100

    Add tests for symmetry

--- test/symmetry.jl
+++ test/symmetry.jl
@@ -1,3 +1,14 @@
+@testitem "rescale" begin
+    using WannierIO: RepMatBand
+
+    d = [1.0 0.0; 0.0 1-1e-4]
+    rep = RepMatBand{2}(1, 1, d)
+    r = Wannier.rescale(rep)
+    ref = [1.0 0.0; 0.0 1.0]
+
+    @test isapprox(r.d, ref)
+end
+
 @testitem "find_wf_symmetry_translations" begin
     using WannierIO, Wannier.Datasets
     # sym = read_isym(dataset"Si2_hse/Si2.isym")
