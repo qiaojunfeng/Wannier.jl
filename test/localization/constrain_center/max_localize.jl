a119ba2b8da7ad5a0e31f4ffe4d93252562bddbd
Junfeng Qiao <qiaojunfeng@outlook.com>
2026-04-06 13:30:53 +0200

    Update Artifacts.toml

--- test/localization/constrain_center/max_localize.jl
+++ test/localization/constrain_center/max_localize.jl
@@ -29,14 +29,8 @@ end
     @test isapprox(G, G_ref; atol=1e-6)
 
     # Test 2nd iteration
-    # This W1 is a random rotation, I use it here so I don't need to store the input AMN matrices as a file.
-    W1 = [
-        0.497264+0.129534im -0.281405-0.540402im 0.0625545+0.237438im -0.516272-0.194678im
-        0.113245-0.532338im 0.47272+0.0873106im -0.546504+0.19511im -0.36623+0.042955im
-        -0.0956088+0.205024im 0.468778+0.234124im 0.563928-0.141122im -0.572565+0.0921779im
-        0.561868-0.269944im 0.350754-0.00970417im 0.451343+0.247661im 0.471665-0.0282528im
-    ]
-    U1 = merge_gauge(U0, W1)
+    U1 = Wannier.max_localize(p, model; max_iter=1)
+    U1 = stack(U1)
 
     fg!(nothing, G, U1)
     d = OnceDifferentiable(x -> fg!(1.0, nothing, x), U1, zero(eltype(real(U1))))
--- test/localization/opt_rotate.jl
+++ test/localization/opt_rotate.jl
@@ -28,13 +28,8 @@ end
     # I am using a looser tolerance here
     @test isapprox(G, G_ref; atol=1e-6)
 
-    # Test 2nd iteration, with a random rotation
-    W1 = [
-        0.497264+0.129534im -0.281405-0.540402im 0.0625545+0.237438im -0.516272-0.194678im
-        0.113245-0.532338im 0.47272+0.0873106im -0.546504+0.19511im -0.36623+0.042955im
-        -0.0956088+0.205024im 0.468778+0.234124im 0.563928-0.141122im -0.572565+0.0921779im
-        0.561868-0.269944im 0.350754-0.00970417im 0.451343+0.247661im 0.471665-0.0282528im
-    ]
+    # Test 2nd iteration
+    W1 = Wannier.opt_rotate(model; max_iter=1)
 
     g!(G, W1)
     d = OnceDifferentiable(f, W1, zero(eltype(real(W1))))
